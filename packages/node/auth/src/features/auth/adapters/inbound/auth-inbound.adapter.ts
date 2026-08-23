ccb242eda79ceeea17e1a1da60f6c041bca1ec13
Jaydeep Vagh <jaydeep.9664920749@gmail.com>
2026-08-23 17:31:32 +0530

    chore(auth/ports): remove empty ports and adapters directory trees

--- packages/node/auth/src/features/auth/adapters/inbound/auth-inbound.adapter.ts
+++
@@ -1,8 +0,0 @@
-export interface IAuthInboundAdapter {
-  handleRequest(
-    method: string,
-    path: string,
-    body?: unknown,
-    headers?: Record<string, string>
-  ): Promise<{ statusCode: number; payload: unknown }>;
-}
--- packages/node/auth/src/features/auth/adapters/outbound/auth-outbound.adapter.ts
+++
@@ -1,3 +0,0 @@
-import type { IAuthOutboundPort } from '../../ports/outbound/auth-outbound.port';
-
-export interface IAuthOutboundAdapter extends IAuthOutboundPort {}
