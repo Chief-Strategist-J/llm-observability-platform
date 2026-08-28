068c8e5722c9131e403832d8d3f6f6125b8b5d73
Jaydeep Vagh <jaydeep.9664920749@gmail.com>
2026-08-28 16:49:23 +0530

    chore: remove unused packages/node/webrtc-client package

--- packages/node/webrtc-client/src/shared/ports/media_port.ts
+++
@@ -1,6 +0,0 @@
-export interface IMediaPort {
-  getLocalStream(constraints: MediaStreamConstraints): Promise<MediaStream>;
-  attachLocalStream(stream: MediaStream, element: HTMLVideoElement): void;
-  attachRemoteStream(stream: MediaStream, element: HTMLVideoElement): void;
-  stopStream(stream: MediaStream): void;
-}
